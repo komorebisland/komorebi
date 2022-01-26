078b3b3a781540b0bc4eb8cae70527ef344dcc5d
Paolo Porcellato <paolo.porcellato@gmail.com>
2022-01-26 08:43:57 +0100

    clean style attribute

--- assets/js/3-city_access.js
+++ assets/js/3-city_access.js
@@ -6,61 +6,49 @@ items = document.getElementsByClassName(itemClassName),
     slide = 0,
     moving = false;
 
-// Next navigation handler
+// Click Sucessivo
 function moveNext() {
-    // Check if moving
     if (!moving) {
-        // If it's the last slide, reset to 0, else +1
-        if (slide === (totalItems - 1)) {
-            slide = 0;
-        } else {
-            slide++;
-        }
-        // Move carousel to updated slide
+        slide = (slide +1 ) % totalItems;
         moveCarouselTo(slide);
     }
 }
-// Previous navigation handler
+// Click Precedente
 function movePrev() {
-    // Check if moving
     if (!moving) {
-        // If it's the first slide, set as the last slide, else -1
         if (slide === 0) {
+            // Vai all'utlima slide
             slide = (totalItems - 1);
         } else {
             slide--;
         }
-
-        // Move carousel to updated slide
         moveCarouselTo(slide);
     }
 }
 function setInitialClasses() {
-    // Targets the previous, current, and next items
-    // This assumes there are at least three items.
+    // l'ultima slide è la precedente alla prima
     items[totalItems - 1].classList.add("prev");
+    // la prima slide è quella inizialmente active
     items[0].classList.add("active");
+    // la seconda slide è la successiva alla prima
     items[1].classList.add("next");
 }
 function moveCarouselTo(slide) {
-    // Check if carousel is moving, if not, allow interaction
     if (!moving) {
-        // temporarily disable interactivity
+        // disabilita le interazioni di click per completare l'animazione
         disableInteraction();
-        // Update the "old" adjacent slides with "new" ones
+        // Aggiorno i precedenti adiacenti prev, next, active
         let newPrevious = slide - 1,
             newNext = slide + 1,
             oldPrevious = slide - 2,
             oldNext = slide + 2;
-        // Test if carousel has more than three items
-        // if ((totalItems - 1) > 3) {
-        // Checks and updates if the new slides are out of bounds
+        
+        // Controllo se le nuove slide sono fuori dagli indici
         if (newPrevious <= 0) {
             oldPrevious = (totalItems - 1);
         } else if (newNext >= (totalItems - 1)) {
             oldNext = 0;
         }
-        // Checks and updates if slide is at the beginning/end
         if (slide === 0) {
             newPrevious = (totalItems - 1);
             oldPrevious = (totalItems - 2);
@@ -70,9 +58,8 @@ function moveCarouselTo(slide) {
             newNext = 0;
             oldNext = 1;
         }
-        // Now we've worked out where we are and where we're going, 
-        // by adding/removing classes we'll trigger the transitions.
-        // Reset old next/prev elements to default classes
+
+        // aggiungendo/rimuovendo classi verrà triggerata l'animazione di transizione
         items[oldPrevious].className = itemClassName;
         if (oldNext < totalItems)
             items[oldNext].className = itemClassName;
@@ -81,15 +68,12 @@ function moveCarouselTo(slide) {
         items[newPrevious].className = itemClassName + " prev";
         items[slide].className = itemClassName + " active";
         items[newNext].className = itemClassName + " next";
-        // }
     }
 }
 function disableInteraction() {
-    // Set 'moving' to true for the same duration as our transition.
-    // (0.5s = 500ms)
-
     moving = true;
-    // setTimeout runs its function once after the given time
+    // Dopo 500ms le interazioni saranno riabilitate
+    // 500ms = durata transizione .transport_carousel_wrapper .means_of_transport
     setTimeout(function () {
         moving = false
     }, 500);
